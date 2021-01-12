3399b138201d24f3c7dab1882ade59e217a2ad9f
宁哈哈 <15705692656@163.com>
2021-01-12 23:39:09 +0800

    修改shell脚本

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@ git pull
 npm install --registry=https://registry.npm.taobao.org/ && npm run build
 
 # 删除容器
-docker rm -f demo1 &> /dev/null
+docker rm -f quirky_williamson &> /dev/null
 
 #启动容器
 docker run -d --restart=on-failure:5\
     -p 8080:80 \
     -v $PWD/dist:/usr/share/nginx/html \
-    --name demo1 nginx
+    --name quirky_williamson nginx
 
 
